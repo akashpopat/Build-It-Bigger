603984ff91f12d58078e3a183951f36de3bf9f2f
Akash Popat <akashpopat@gmail.com>
2016-11-08 11:38:56 +0530

    feat: Added Java lib to get jokes

---
+++ javajokes/src/main/java/com/akashpopat/Jokes.java
@@ -0,0 +1,7 @@
+package com.akashpopat;
+
+public class Jokes {
+    public String getJoke() {
+        return "Alcohol is a perfect solvent: It dissolves marriages, families and careers.";
+    }
+}
